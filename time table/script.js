561c2f16591c2ed9958fe8a42b1947ef96858054
nk8713694 <nk8713694@gmail.com>
2020-07-19 09:29:16 +0530

    web dev

---
+++ time table/script.js
@@ -0,0 +1,23 @@
+function printThis() {
+    console.log('this = ', this)
+    return this
+  }
+  
+  let obj1 = {
+    a: NaN,
+    b: 20,
+    fn: printThis
+  }
+  // obj1.fn ??
+  
+  let obj2 = {
+    p: 'asds',
+    q: 'afadg',
+    fun: printThis
+  }
+  
+  
+  let obj3 = {}
+  obj3.x = 12
+  obj3.y =  'asd'
+  obj3.z = true
